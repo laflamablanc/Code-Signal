a1d6f3f00353f70ef2c375e1f9b9bb2f0bd61a8e
Sean LaFlam <sean.laflam@gmail.com>
2021-02-23 17:21:11 -0500

    Working

--- Arrays/Practice.js
+++ Arrays/Practice.js
@@ -1,14 +1,28 @@
-function firstDuplicate(a) {
-    let dictionary = {}
-    for (let i =0; i<a.length; i++){
-        if (dictionary[a[i]]){
-            return a[i]
-        }
-        dictionary[a[i]] = true
+// function firstDuplicate(a) {
+//     let dictionary = {}
+//     for (let i =0; i<a.length; i++){
+//         if (dictionary[a[i]]){
+//             return a[i]
+//         }
+//         dictionary[a[i]] = true
+//     }
+//     return -1
+// }
+
+// array = [2,1,3,5,3,2]
+
+// console.log(firstDuplicate(array))
+
+function centuryFromYear(year) {
+    if (year.length === 2){
+        year = "00" + year.toString()
+    } else if (year.length ===3 ){
+        year = "0" + year.toString()
     }
-    return -1
+    
+    console.log(year)
 }
 
-array = [2,1,3,5,3,2]
+centuryFromYear(10)
 
-console.log(firstDuplicate(array))
+centuryFromYear(570)
