4cca2717f611063aa1b16725b24d53ba50d91e65
Sean LaFlam <sean.laflam@gmail.com>
2021-02-23 16:56:39 -0500

    Working

--- Arrays/Practice.js
+++ Arrays/Practice.js
@@ -1,11 +1,14 @@
 function firstDuplicate(a) {
     let dictionary = {}
-    a.forEach(num => {
-        if (dictionary[num]){
-            return num
+    for (let i =0; i<a.length; i++){
+        if (dictionary[a[i]]){
+            return a[i]
         }
-        dictionary[num] = true
-        console.log(dictionary)
-    })
+        dictionary[a[i]] = true
+    }
     return -1
 }
+
+array = [2,1,3,5,3,2]
+
+console.log(firstDuplicate(array))
